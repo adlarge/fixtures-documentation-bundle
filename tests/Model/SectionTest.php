511ecbc0506cbeb28b5882a4b18d2afb00885524
Arnaud Large <alarge@eurogage.com>
2019-07-30 07:39:13 +0200

    link: Add links tests

--- tests/Model/SectionTest.php
+++ tests/Model/SectionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Model;
 
+use Adlarge\FixturesDocumentationBundle\Model\Fixture;
 use PHPUnit\Framework\TestCase;
 use Adlarge\FixturesDocumentationBundle\Model\Section;
 use Adlarge\FixturesDocumentationBundle\Exception\DuplicateFixtureException;
@@ -29,14 +30,17 @@ class SectionTest extends TestCase
     public function testAddFixture(): void
     {
         $section = new Section('title');
-        $section->addFixture(['id' => '1', 'name' => 'fixture1']);
-        $section->addFixture(['id' => '2', 'name' => 'fixture2']);
+        $fixture1 = $section->addFixture(['id' => '1', 'name' => 'fixture1']);
+        $fixture2 = $section->addFixture(['id' => '2', 'name' => 'fixture2']);
 
-        $expectedFixtures = [
-            ['id' => '1', 'name' => 'fixture1'],
-            ['id' => '2', 'name' => 'fixture2']
-        ];
-        $this->assertSame($expectedFixtures, $section->getFixtures());
+        $this->assertInstanceOf(Fixture::class, $fixture1);
+        $this->assertInstanceOf(Fixture::class, $fixture2);
+        $this->assertSame('title-1', $fixture1->getId());
+        $this->assertSame('title-2', $fixture2->getId());
+        $this->assertSame(['id' => '1', 'name' => 'fixture1'], $fixture1->getData());
+        $this->assertSame(['id' => '2', 'name' => 'fixture2'], $fixture2->getData());
+        $this->assertSame([], $fixture1->getLinks());
+        $this->assertSame([], $fixture2->getLinks());
 
         $expectedHeaders = ['id', 'name'];
         $this->assertSame($expectedHeaders, $section->getHeaders());
